27a1f38095b88ecd5989ffb006d868fab9b4126b
Ujjwal <bansalujjwal2002@gmail.com>
2024-04-06 18:23:48 +0530

    data is added

--- index.js
+++ index.js
@@ -11,6 +11,53 @@ const app = express()
 // setting port
 const port = 4000
 
+let data={
+    "login": "octocat",
+    "id": 1,
+    "node_id": "MDQ6VXNlcjE=",
+    "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/octocat",
+    "html_url": "https://github.com/octocat",
+    "followers_url": "https://api.github.com/users/octocat/followers",
+    "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+    "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+    "organizations_url": "https://api.github.com/users/octocat/orgs",
+    "repos_url": "https://api.github.com/users/octocat/repos",
+    "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/octocat/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "monalisa octocat",
+    "company": "GitHub",
+    "blog": "https://github.com/blog",
+    "location": "San Francisco",
+    "email": "octocat@github.com",
+    "hireable": false,
+    "bio": "There once was...",
+    "twitter_username": "monatheoctocat",
+    "public_repos": 2,
+    "public_gists": 1,
+    "followers": 20,
+    "following": 0,
+    "created_at": "2008-01-14T04:33:35Z",
+    "updated_at": "2008-01-14T04:33:35Z",
+    "private_gists": 81,
+    "total_private_repos": 100,
+    "owned_private_repos": 100,
+    "disk_usage": 10000,
+    "collaborators": 8,
+    "two_factor_authentication": true,
+    "plan": {
+      "name": "Medium",
+      "space": 400,
+      "private_repos": 20,
+      "collaborators": 0
+    }
+  }
+
 
 // Creating methods of get/post/put etc.
 
@@ -30,10 +77,14 @@ app.get('/youtube',(req , res)=>{
     res.send("<h2>Welcome to youtube</h2>")
 })
 
+app.get('/data',(req , res)=>{
+    res.json(data)
+})
+
 
 
 // listening our request by using listen() of express
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+  console.log(`Example app listening on port http://localhost:${process.env.PORT}`)
 })
